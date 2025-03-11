e618d7a534b7f350d289f19c21e5d1bcb46da37a
Mama <2320162@claurendeau.qc.ca>
2025-03-10 21:08:21 -0400

    ajout de la classe utilitaire HibernateUtil pour gérer SessionFactory

---
+++ src/main/java/ca/cal/tp2/utilis/HibernateUtil.kt
@@ -0,0 +1,24 @@
+package ca.cal.tp1.utilis
+
+import org.hibernate.SessionFactory
+import org.hibernate.cfg.Configuration
+
+object HibernateUtil {
+    // ✅ La méthode doit être static
+    val sessionFactory: SessionFactory? = buildSessionFactory() // ✅ Correct
+
+    private fun buildSessionFactory(): SessionFactory {
+        try {
+            return Configuration().configure("hibernate.cfg.xml").buildSessionFactory()
+        } catch (ex: Throwable) {
+            System.err.println("❌ Erreur Hibernate : $ex")
+            throw ExceptionInInitializerError(ex)
+        }
+    }
+
+    fun shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close()
+        }
+    }
+}
