172668a6bd6d1cec75c6c5e799d6481dbed37123
Mama <2320162@claurendeau.qc.ca>
2025-03-10 21:31:52 -0400

    Le préposé peut ajouter un documents

---
+++ src/main/java/ca/cal/tp2/Service/PreposeService.java
@@ -0,0 +1,39 @@
+package ca.cal.tp2.Service;
+
+import ca.cal.tp2.DTOs.DocumentDTO;
+import ca.cal.tp2.DTOs.EmprunteurDTO;
+import ca.cal.tp2.DTOs.PreposeDTO;
+import ca.cal.tp2.Dao.AmendeDAO;
+import ca.cal.tp2.Dao.DocumentDAO;
+import ca.cal.tp2.Dao.EmpruntDAO;
+import ca.cal.tp2.Dao.UtilisateurDAO;
+import ca.cal.tp2.Modeles.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PreposeService {
+    private final EmpruntDAO empruntDAO;
+    private final UtilisateurDAO utilisateurDAO;
+    private final DocumentDAO documentDAO;
+    private final AmendeDAO amendeDAO;
+    private static final BigDecimal MONTANT_AMENDE_PAR_JOUR = BigDecimal.valueOf(0.25);
+
+    public PreposeService(EmpruntDAO empruntDAO, UtilisateurDAO utilisateurDAO, DocumentDAO documentDAO, AmendeDAO amendeDAO) {
+        this.empruntDAO = empruntDAO;
+        this.utilisateurDAO = utilisateurDAO;
+        this.documentDAO = documentDAO;
+        this.amendeDAO = amendeDAO;
+    }
+
+    public void ajouterDocument(DocumentDTO documentDTO) {
+        Document document = MapperService.versDocumentEntite(documentDTO);
+        documentDAO.ajouter(document);
+
+    }
+
+}
